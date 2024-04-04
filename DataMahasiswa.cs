b8bb679f7f865bdca1f870881ad182748f074a51
sorayahaidars <154647224+sorayahaidars@users.noreply.github.com>
2024-04-04 12:01:56 +0700

    Add files via upload
    "menambahkan class DataMahasiswa"

---
+++ DataMahasiswa.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace JurnalMod7_Kelompok_1
+{
+    class MahasiswaData
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public address address { get; set; }
+
+        public List<kelas> courses { get; set; }
+    }
+
+    class address
+    {
+        public string streetAddress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+    }
+    class kelas
+    {
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public static class DataMahasiswa
+    {
+        public static void ReadJSON()
+        {
+            string jsonString = File.ReadAllText("D:\JurnalMod7_Kelompok_1\jurnal7_1_1302223006.json");
+            MahasiswaData mahasiswa = JsonSerializer.Deserialize<MahasiswaData>(jsonString);
+
+            Console.WriteLine("Nama : " + mahasiswa.firstName + " " + mahasiswa.lastName + " dengan gender " + mahasiswa.gender);
+            Console.WriteLine("Umur : " + mahasiswa.age);
+            Console.WriteLine("Alamat : " + mahasiswa.address.streetAddress + " " + mahasiswa.address.city + " " + mahasiswa.address.state);
+            Console.WriteLine("Mengambil mata kuliah : ");
+            for (int i = 0; i < mahasiswa.courses.Count; i++)
+            {
+                Console.WriteLine(mahasiswa.courses[i].code + " " + mahasiswa.courses[i].name);
+            }
+        }
+    }
+}
